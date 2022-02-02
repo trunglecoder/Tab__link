33c941566d043539ca97435290735a337616dba2
trunglecoder <93107952+trunglecoder@users.noreply.github.com>
2022-02-02 15:03:24 +0700

    first commit

---
+++ answer.js
@@ -0,0 +1,382 @@
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+// const tabs = $$(".tab-item");
+// const panes = $$(".tab-pane");
+
+// const tabActive = $(".tab-item.active");
+// const line = $(".tabs .line");
+
+// requestIdleCallback(function () {
+//   line.style.left = tabActive.offsetLeft + "px";
+//   line.style.width = tabActive.offsetWidth + "px";
+// });
+
+// tabs.forEach((tab, index) => {
+//   const pane = panes[index];
+
+//   tab.onclick = function () {
+//     $(".tab-item.active").classList.remove("active");
+//     $(".tab-pane.active").classList.remove("active");
+
+//     line.style.left = this.offsetLeft + "px";
+//     line.style.width = this.offsetWidth + "px";
+
+//     this.classList.add("active");
+//     pane.classList.add("active");
+//   };
+// });
+
+
+
+
+
+
+// {
+//     Thông Trần
+//     238 bạn chung
+//     Nhắn tin
+
+//     Nguyệt An
+//     158 bạn chung
+//     Nhắn tin
+
+//     <!-- Hoàng Châu
+//     54 bạn chung
+//     Nhắn tin
+
+//     Nguyễn Lê TNguyên
+//     59 bạn chung
+//     Nhắn tin
+
+//     Phạm Uyên
+//     17 bạn chung
+//     Nhắn tin
+
+//     Nguyễn Hồ Vân Nhi
+//     309 bạn chung
+//     Nhắn tin
+
+//     Nguyên Hưng
+//     222 bạn chung
+//     Nhắn tin
+
+//     Vy Võ
+//     78 bạn chung
+//     Nhắn tin
+
+//     Thư Nguyễn
+//     51 bạn chung
+//     Nhắn tin
+
+//     Minh Huy TRương
+//     227 bạn chung
+//     Nhắn tin
+
+//     Trâm Nguyễn
+//     47 bạn chung
+//     Nhắn tin
+
+//     Ngọc Thư
+//     86 bạn chung
+//     Nhắn tin
+
+//     Nguyễn Thư
+//     72 bạn chung
+//     Nhắn tin
+
+//     Như Lê
+//     105 bạn chung
+//     Nhắn tin
+
+//     Ánh Nguyễn
+//     31 bạn chung
+//     Nhắn tin
+
+//     Phương Nhi
+//     48 bạn chung
+//     Nhắn tin
+
+//     Nguyễn Thư
+//     138 bạn chung
+//     Thêm bạn bè
+
+//     Huy Bảo
+//     2 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Thanh Vinh
+//     Thêm bạn bè
+
+//     Trung Le
+//     Thêm bạn bè
+
+//     Đình Hưng Trương
+//     16 bạn chung
+//     Thêm bạn bè
+
+//     Lan Anh Le
+//     Thêm bạn bè
+
+//     Trang Thạch
+//     37 bạn chung
+//     Thêm bạn bè
+
+//     Bảo Phạm
+//     79 bạn chung
+//     Thêm bạn bè
+
+//     Tiên Cát
+//     25 bạn chung
+//     Thêm bạn bè
+
+//     Trần Công Danh
+//     72 bạn chung
+//     Hủy yêu cầu
+
+//     Hoàng Phương Uyên Nguyễn
+//     93 bạn chung
+//     Thêm bạn bè
+
+//     Van Minh
+//     210 bạn chung
+//     Thêm bạn bè
+
+//     Đình Hưng Trương
+//     15 bạn chung
+//     Thêm bạn bè
+
+//     Đặng Ngọc Trân
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Đức Nguyễn
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Phương Hà Lê Nguyễn
+//     2 bạn chung
+//     Thêm bạn bè
+
+//     Hoài Trang
+//     20 bạn chung
+//     Thêm bạn bè
+
+//     Dao Pham
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Yên Nhii
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Dư Nguyên Phúc
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Phương Thảo
+//     19 bạn chung
+//     Thêm bạn bè
+
+//     Tâm Đặng
+//     Thêm bạn bè
+
+//     Đặng Hồng
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Trọng Tài
+//     70 bạn chung
+//     Thêm bạn bè
+
+//     Thảo Trần
+//     3 bạn chung
+//     Thêm bạn bè
+
+//     Hoàng Hải Yến
+//     25 bạn chung
+//     Thêm bạn bè
+
+//     Trần Hoa
+//     Thêm bạn bè
+
+//     Linh Chi
+//     17 bạn chung
+//     Thêm bạn bè
+
+//     Như Quỳnh
+//     33 bạn chung
+//     Thêm bạn bè
+
+//     Phạm Thanh Huy
+//     48 bạn chung
+//     Thêm bạn bè
+
+//     Duc Hoa Tran Nguyen
+//     44 bạn chung
+//     Thêm bạn bè
+
+//     Khoa Lê
+//     77 bạn chung
+//     Thêm bạn bè
+
+//     Hải Tiên
+//     10 bạn chung
+//     Thêm bạn bè
+
+//     Phạm Phương Thảo
+//     33 bạn chung
+//     Thêm bạn bè
+
+//     Nguyên Thảo
+//     2 bạn chung
+//     Thêm bạn bè
+
+//     Vinh Thien Le Hoang
+//     36 bạn chung
+//     Xác nhận lời mời kết bạn
+
+//     Honghung Tran
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Trần Tường Vy
+//     19 bạn chung
+//     Thêm bạn bè
+
+//     Lê Hoàng
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Tam Le
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Lê Chung
+//     19 bạn chung
+//     Thêm bạn bè
+
+//     Quan Le
+//     23 bạn chung
+//     Thêm bạn bè
+
+//     Bao Pham
+//     188 bạn chung
+//     Xác nhận lời mời kết bạn
+
+//     Xuân Huyền
+//     37 bạn chung
+//     Thêm bạn bè
+
+//     Phan Hoàng
+//     125 bạn chung
+//     Thêm bạn bè
+
+//     Hữu Bảo
+//     72 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Ngọc Ly Na
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Đỗ Hy Hiền
+//     60 bạn chung
+//     Thêm bạn bè
+
+//     Lê Vân Thùy
+//     37 bạn chung
+//     Thêm bạn bè
+
+//     Lê Phương Thuỳ
+//     36 bạn chung
+//     Hủy yêu cầu
+
+//     Lê Uyển Nhi
+//     Thêm bạn bè
+
+//     Nguyễn Quang Khang
+//     16 bạn chung
+//     Thêm bạn bè
+
+//     Quynh Chi Duong
+//     14 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Hữu Ngọc
+//     24 bạn chung
+//     Hủy yêu cầu
+
+//     Khánh Phan
+//     283 bạn chung
+//     Thêm bạn bè
+
+//     Quynh Truong
+//     Thêm bạn bè
+
+//     Khanh Lê
+//     67 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Ngọc Đức
+//     6 bạn chung
+//     Thêm bạn bè
+
+//     Huy Bảo
+//     64 bạn chung
+//     Thêm bạn bè
+
+//     Ngô Thành Phát
+//     18 bạn chung
+//     Thêm bạn bè
+
+//     Trương Quỳnh
+//     54 bạn chung
+//     Thêm bạn bè
+
+//     Uyen Phuong Tran
+//     85 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Thanh An
+//     14 bạn chung
+//     Thêm bạn bè
+
+//     Tiên Phạm
+//     1 bạn chung
+//     Thêm bạn bè
+
+//     Bảo Ngọcc
+//     20 bạn chung
+//     Thêm bạn bè
+
+//     Trần Chung
+//     50 bạn chung
+//     Thêm bạn bè
+
+//     Hạ Như
+//     30 bạn chung
+//     Thêm bạn bè
+
+//     Chu Lộc
+//     23 bạn chung
+//     Thêm bạn bè
+
+//     Uyên Nguyễn
+//     14 bạn chung
+//     Thêm bạn bè
+
+//     Chí Thành Lê
+//     64 bạn chung
+//     Thêm bạn bè
+
+//     Tấn Trường
+//     26 bạn chung
+//     Thêm bạn bè
+
+//     Nguyễn Thị Ngọc Vy
+//     51 bạn chung
+//     Thêm bạn bè
+
+//     -->
+// }
